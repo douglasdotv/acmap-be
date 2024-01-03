2bea79fbbb9372058288ee684b0daca6e74afa4e
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-03 10:58:08 -0300

    Add AccidentController with endpoint to get all accidents

--- src/main/java/br/com/dv/acmap/controller/AccidentController.java
+++ src/main/java/br/com/dv/acmap/controller/AccidentController.java
@@ -1,2 +1,28 @@
-package br.com.dv.acmap.controller;public class AccidentController {
+package br.com.dv.acmap.controller;
+
+import br.com.dv.acmap.dto.AccidentDTO;
+import br.com.dv.acmap.service.AccidentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/accidents")
+public class AccidentController {
+
+    private final AccidentService accidentService;
+
+    public AccidentController(AccidentService accidentService) {
+        this.accidentService = accidentService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AccidentDTO>> getAllAccidents() {
+        List<AccidentDTO> accidents = accidentService.getAllAccidents();
+        return ResponseEntity.ok(accidents);
+    }
+
 }
