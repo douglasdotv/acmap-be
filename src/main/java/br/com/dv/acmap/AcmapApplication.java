554b84c0be1b9c2c8dff4b7065ed172aefd3a69b
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-03 09:59:02 -0300

    Initial commit
    Generated Spring Boot project and added .gitignore

---
+++ src/main/java/br/com/dv/acmap/AcmapApplication.java
@@ -0,0 +1,13 @@
+package br.com.dv.acmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcmapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/dv/acmap/AcmapApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dv.acmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
