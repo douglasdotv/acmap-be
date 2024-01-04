e7031255f7c4688bd50e528022e43c836622931d
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-04 15:36:19 -0300

    Enable CORS for AccidentController

--- src/main/java/br/com/dv/acmap/controller/AccidentController.java
+++ src/main/java/br/com/dv/acmap/controller/AccidentController.java
@@ -3,12 +3,14 @@ package br.com.dv.acmap.controller;
 import br.com.dv.acmap.dto.AccidentDTO;
 import br.com.dv.acmap.service.AccidentService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/accidents")
 public class AccidentController {
