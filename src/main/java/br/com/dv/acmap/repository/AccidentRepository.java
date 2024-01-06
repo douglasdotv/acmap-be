409fa7664eafe2735a4b7ccc18525508be77b1ee
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-06 17:47:39 -0300

    Add query to filter accidents by op, ac and cat
    Added, in AccidentRepository, a custom query to filter accidents by operator, aircraft type and category

--- src/main/java/br/com/dv/acmap/repository/AccidentRepository.java
+++ src/main/java/br/com/dv/acmap/repository/AccidentRepository.java
@@ -3,6 +3,7 @@ package br.com.dv.acmap.repository;
 import br.com.dv.acmap.entity.Accident;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,5 +14,17 @@ public interface AccidentRepository extends JpaRepository<Accident, Integer> {
 
     @Query("SELECT DISTINCT a.aircraftType FROM Accident a ORDER BY a.aircraftType")
     List<String> findDistinctAircraftTypes();
+
+    @Query("""
+            SELECT a FROM Accident a LEFT JOIN a.categories c WHERE
+            (:fatalities IS NULL OR a.fatalities >= :fatalities) AND
+            (:operator = '' OR a.operator = :operator) AND
+            (:aircraftType = '' OR a.aircraftType LIKE CONCAT('%', :aircraftType, '%')) AND
+            (:category = '' OR c.category = :category)
+            """)
+    List<Accident> findBySearchFilters(@Param("fatalities") Integer fatalities,
+                                       @Param("operator") String operator,
+                                       @Param("aircraftType") String aircraftType,
+                                       @Param("category") String category);
 
 }
