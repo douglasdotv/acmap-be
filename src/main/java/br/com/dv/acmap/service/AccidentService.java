034fd98af71bb4d3e6096f93018b0af9ea1214b2
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-06 17:49:39 -0300

    Implement method to filter accidents by op, ac and cat
    Implemented searchAccidents method in back end service layer to handle the form/POST request

--- src/main/java/br/com/dv/acmap/service/AccidentService.java
+++ src/main/java/br/com/dv/acmap/service/AccidentService.java
@@ -1,12 +1,15 @@
 package br.com.dv.acmap.service;
 
 import br.com.dv.acmap.dto.AccidentDTO;
+import br.com.dv.acmap.dto.SearchFormDTO;
 
 import java.util.List;
 
 public interface AccidentService {
 
     List<AccidentDTO> getAllAccidents();
+
+    List<AccidentDTO> searchAccidents(SearchFormDTO form);
 
     List<String> getOperators();
 
--- src/main/java/br/com/dv/acmap/service/AccidentServiceImpl.java
+++ src/main/java/br/com/dv/acmap/service/AccidentServiceImpl.java
@@ -1,6 +1,7 @@
 package br.com.dv.acmap.service;
 
 import br.com.dv.acmap.dto.AccidentDTO;
+import br.com.dv.acmap.dto.SearchFormDTO;
 import br.com.dv.acmap.entity.Accident;
 import br.com.dv.acmap.mapper.AccidentMapper;
 import br.com.dv.acmap.repository.AccidentCategoryRepository;
@@ -27,6 +28,16 @@ public class AccidentServiceImpl implements AccidentService {
     @Override
     public List<AccidentDTO> getAllAccidents() {
         List<Accident> accidents = accidentRepository.findAll();
+        return accidentMapper.toAccidentDTOs(accidents);
+    }
+
+    public List<AccidentDTO> searchAccidents(SearchFormDTO form) {
+        List<Accident> accidents = accidentRepository.findBySearchFilters(
+                form.fatalities(),
+                form.operator(),
+                form.aircraftType(),
+                form.accidentCategory()
+        );
         return accidentMapper.toAccidentDTOs(accidents);
     }
 
