a0afc618e19b5da5270db1ffd881e1fe6f211629
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-06 09:34:09 -0300

    Add queries for distinct op, ac and cat
    Added queries to retrieve distinct operators and aircraft types to AccidentRepository. Added query to retrieve distinct categories to AccidentCategoryRepository.

--- src/main/java/br/com/dv/acmap/repository/AccidentRepository.java
+++ src/main/java/br/com/dv/acmap/repository/AccidentRepository.java
@@ -2,6 +2,16 @@ package br.com.dv.acmap.repository;
 
 import br.com.dv.acmap.entity.Accident;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AccidentRepository extends JpaRepository<Accident, Integer> {
+
+    @Query("SELECT DISTINCT a.operator FROM Accident a ORDER BY a.operator")
+    List<String> findDistinctOperators();
+
+    @Query("SELECT DISTINCT a.aircraftType FROM Accident a ORDER BY a.aircraftType")
+    List<String> findDistinctAircraftTypes();
+
 }
