c6c3e2839a161c99fec171beeb47e91599226c95
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-08 10:20:55 -0300

    Replace annotation with config class for CORS
    Replaced @CrossOrigin annotation with a configuration class for setting up a global CORS policy

--- src/main/java/br/com/dv/acmap/controller/AccidentController.java
+++ src/main/java/br/com/dv/acmap/controller/AccidentController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/accidents")
 public class AccidentController {
