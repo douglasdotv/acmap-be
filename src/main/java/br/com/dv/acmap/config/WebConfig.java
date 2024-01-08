70f14a2cf1d42965c799b4dd8f4427d45b8d7cff
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-08 10:22:59 -0300

    Use environment variable for allowed origin

--- src/main/java/br/com/dv/acmap/config/WebConfig.java
+++ src/main/java/br/com/dv/acmap/config/WebConfig.java
@@ -1,5 +1,6 @@
 package br.com.dv.acmap.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${CLIENT_URL:http://localhost:4200}")
+    private String clientUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(clientUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
